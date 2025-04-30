27f83528b3014013df5cece6bc0d4bfbeaf7774d
Yoshua Guido Romero <yosh.701@gmail.com>
2025-04-30 17:38:21 -0600

    S1

--- SDJavaScriptSDK/com.mirabox.streamdock.xxx.sdPlugin/plugin/index.js
+++ SDJavaScriptSDK/com.mirabox.streamdock.xxx.sdPlugin/plugin/index.js
@@ -6,10 +6,21 @@ const plugin = new Plugins("xxx");
 // 操作一
 plugin.action1 = new Actions({
     default: {},
-    _willAppear({ context }) {
-        window.socket.setTitle(context, "Hello world!");
+    _willAppear({ context, payload }) {
+        let count = 0;
+        if ("count" in payload.settings) {
+            count = payload.settings.count;
+        } else {
+            window.socket.setSettings(context, { count: count });
+        }
+        window.socket.setTitle(context, count + '');
     },
     _willDisappear({ context }) { },
+    keyUp(data) {
+        let count = data.payload.settings.count + 1;
+        window.socket.setSettings(data.context, { count: count })
+        window.socket.setTitle(data.context, count + '');
+    },
     dialRotate(data) {//旋钮旋转
         console.log(data);
     },
